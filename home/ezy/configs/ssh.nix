bece6676ba4e35925a4e6dc2ec09df42d8708ff2
EzYDark <ezydark@protonmail.com>
2024-01-17 20:07:54 +0100

    Temp fix for "~/.ssh/config" bad owner and permission bug

--- home/ezy/configs/ssh.nix
+++ home/ezy/configs/ssh.nix
@@ -36,5 +36,10 @@
     gpg.enable = true;
   };
 
+  home.file.".ssh/config" = {
+    target = ".ssh/config_source";
+    onChange = ''cat ~/.ssh/config_source > ~/.ssh/config && chmod 400 ~/.ssh/config'';
+  };
+
   services.ssh-agent.enable = true;
 }

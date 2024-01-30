a8b4770e105fd0ee7c3ec5a68415ad02bd9c90fc
EzYDark <ezydark@protonmail.com>
2024-01-31 00:16:00 +0100

    Added custom cache for faster builds

--- system/configs/nix_config.nix
+++ system/configs/nix_config.nix
@@ -12,5 +12,18 @@
     optimise = {
       automatic = true;
     };
+    
+    trusted-users = ["ezy"];
+    substituters = [
+      "https://cache.nixos.org"
+      "https://nix-community.cachix.org"
+    ];
+
+    trusted-public-keys = [
+      # the default public key of cache.nixos.org, it's built-in, no need to add it here
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
   };
 }
